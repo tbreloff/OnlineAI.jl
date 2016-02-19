4a8768c0dc39ad00aa404b3029bff04a4398d007
Thomas Breloff <tom@breloff.com>
2016-02-18 19:51:17 -0500

    circuits

--- src/circuits/viz.jl
+++ src/circuits/viz.jl
@@ -1,5 +1,5 @@
 
-import Graphs, GraphLayout
+import Graphs, GraphLayout, FixedSizeArrays 
 
 """
 create x/y lists of edge coordinates given x/y lists of vertex coordinates
@@ -38,6 +38,34 @@ function create_graph(net::Circuit)
     end
     g
 end
+
+
+# # this code will take a start and end point and return points on the bezier curve between them
+function get_curve_from_centers(c1::P2, c2::P2, yoffset::Real)
+    offset = P2(0, yoffset)
+    BezierCurve(c1 + offset, c2 - offset)
+end
+
+function get_circuit_points(net::Circuit; yoffset = -0.2)
+
+    # populate vectors with the nodes
+    n = length(net)
+    x = 2rand(n)-1
+    y = collect(1.:n)
+    tags = Symbol[node.tag for node in net]
+    types = fill(Node, n)
+    
+    # now add the gate info
+    for (i,node) in enumerate(net), gate in node.gates_in
+        push!(x, x[i])
+        push!(y, y[i] + yoffset)
+        push!(tags, gate.tag)
+        push!(types, Gate)
+    end
+
+    x, y, tags, types
+end
+
 
 
 function Plots._apply_recipe(d::Dict, net::Circuit; kw...)
